3cee0a6b4987f39755ccb52ecd8084d6ae482ef2
haythmelmagraby <haythmelmagraby@gmail.com>
2024-06-20 09:52:44 +0300

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+"""0. How many subs?"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    the_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client_agent = {'User-agent': 'haythm ubuntu 20.2'}
+    res = requests.get(the_url, headers=client_agent, allow_redircts=False)
+    try:
+        return res.json().get('data').get('subscribers')
+    except Exception:
+        return 0
